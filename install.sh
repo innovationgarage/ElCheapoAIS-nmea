0cd83635198095a125d94b155a9336ac2558f9d1
Egil <egil.moller@freecode.no>
2019-12-11 11:08:17 +0100

    Configure network manager if it's not setup already

--- install.sh
+++ install.sh
@@ -39,7 +39,21 @@ echo Installing dependencies
 apt update
 
 # Watchdog: https://forum.armbian.com/topic/2898-how-to-install-enable-and-start-watchdog-in-h3/
-apt install -y python3 python3-pip python3-setuptools python3-dev git gcc openssh-server openssh-client bash libdbus-1-dev libglib2.0-dev libcairo2-dev watchdog overlayroot
+apt install -y python3
+apt install -y python3-pip
+apt install -y python3-setuptools
+apt install -y python3-dev
+apt install -y git
+apt install -y gcc
+apt install -y openssh-server
+apt install -y openssh-client
+apt install -y bash
+apt install -y libdbus-1-dev
+apt install -y libglib2.0-dev
+apt install -y libcairo2-dev
+apt install -y watchdog
+apt install -y overlayroot
+apt install -y network-manager
 
 mkdir -p /var/log/elcheapoais
 mkdir -p /usr/local/bin
@@ -105,6 +119,17 @@ sed -i -e "s+#\? *PasswordAuthentication *yes+PasswordAuthentication no+g" /etc/
 # Throttling: https://forum.armbian.com/topic/9255-orangepi-pc-cpu-frequency/?tab=comments#comment-69278
 # Measure temperature: https://forum.armbian.com/topic/3779-orangepi-zero-high-temperature/
 sed -i -e "s+MAX_SPEED=.*+MAX_SPEED=480000+g" /etc/default/cpufrequtils
+
+if [ -e /usr/sbin/netplan ]; then
+  cat > /etc/netplan/01-network-manager-all.yaml <<EOF
+# Let NetworkManager manage all devices on this system
+network:
+  version: 2
+  renderer: NetworkManager
+EOF
+  netplan generate
+  netplan apply
+fi
 
 # https://docs.armbian.com/User-Guide_Advanced-Features/#how-to-freeze-your-filesystem
 [ "$ARG_no_readonly" ] || echo 'overlayroot="tmpfs"' >> /etc/overlayroot.conf
